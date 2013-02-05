bbfe7de25e12d3fa1d82f7482a18ac0c786a0a47
Anton Gladky <gladky.anton@gmail.com>
2013-02-05 09:14:31 +0100

    Add config-file.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,22 @@
 #include <boost/program_options.hpp>
 
+
 #include <iostream>
+#include <algorithm>
 #include <iterator>
 #include <Eigen/Dense>
+using namespace std;
+
+
+
+// A helper function to simplify the main part.
+template<class T>
+ostream& operator<<(ostream& os, const vector<T>& v)
+{
+    copy(v.begin(), v.end(), ostream_iterator<T>(os, " ")); 
+    return os;
+}
+
 
 namespace po = boost::program_options;
 namespace eig= Eigen;
@@ -15,18 +29,22 @@ int main(int ac, char* av[])
         po::options_description desc("Allowed options");
         desc.add_options()
             ("help", "produce help message")
+            ("config,c", po::value< vector<string> >(), "configuration file")
             ("x-center", po::value<double>()->default_value(0.0), "set x-center coordinate")
             ("y-center", po::value<double>()->default_value(0.0), "set y-center coordinate")
             ("z-center", po::value<double>()->default_value(0.0), "set z-center coordinate")
             ("Din", po::value<double>()->default_value(0.0), "set internal diameter of the rheometer")
             ("Dout", po::value<double>()->default_value(0.0), "set external diameter of the rheometer")
-            ("RadialSec", po::value<int>()->default_value(0), "set numer of sections, in which the rheometer will be divided in radial direction")
-            ("ZSec", po::value<int>()->default_value(0), "set numer of sections, in which the rheometer will be divided in z-direction")
+            ("SecRadial", po::value<int>()->default_value(0), "set numer of sections, in which the rheometer will be divided in radial direction")
+            ("SecZ", po::value<int>()->default_value(0), "set numer of sections, in which the rheometer will be divided in z-direction")
         ;
-
+        
+        po::positional_options_description p;
+        p.add("config", -1);
         po::variables_map vm;        
-        po::store(po::parse_command_line(ac, av, desc), vm);
-        po::notify(vm);    
+        po::store(po::command_line_parser(ac, av).
+        options(desc).positional(p).run(), vm);
+        po::notify(vm);  
 
         if (vm.count("help")) {
             cout << desc << "\n";
@@ -58,7 +76,12 @@ int main(int ac, char* av[])
                  << vm["Dout"].as<double>() << ".\n";
         }
         
-        
+        if (vm.count("config"))
+        {
+            cout << "config file is: " 
+                 << vm["config"].as< vector<string> >() << "\n";
+        }
+
     }
     catch(exception& e) {
         cerr << "error: " << e.what() << "\n";
