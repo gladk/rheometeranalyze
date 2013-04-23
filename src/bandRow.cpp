eea8a1863e85ce123141c63215a16a8946696ed6
Anton Gladky <gladky.anton@gmail.com>
2013-04-23 16:45:22 +0200

    Fix segmentation fault.

--- src/bandRow.cpp
+++ src/bandRow.cpp
@@ -82,7 +82,7 @@ void bandRow::fillBands (){
         int bZ = getBandZ(partTemp->height());
         int bN = bZ*(_cfg->SecRadial()) + bR;
         
-        if (bR>=0 and bZ>=0 and bN>=0 and bN<=_bandAll.size()) {
+        if (bR>=0 and bZ>=0 and bN>=0 and bN<_bandAll.size()) {
           _bandAll[bN]->addParticle(partTemp);
         } else {
           _pRow[i]->disable(z);    // Disable and remove particle, if they are out of bands
