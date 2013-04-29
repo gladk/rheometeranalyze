1ba37b7ccbdf45bf6acb7f3d95e68f5e8ec65005
Anton Gladky <gladky.anton@gmail.com>
2013-04-29 09:24:22 +0200

    Export some more fields.

--- src/export.cpp
+++ src/export.cpp
@@ -434,7 +434,12 @@ void exportclass::gnuplotSchearRate() {
   
   ofstream myfileG (_fileNameG.c_str());
   myfileG << "#000_id\t001_r\t002_z\t003_rPos\t004_zPos\t005_H\t006_W\t007_PartN\t";
-  myfileG << "008_vol\t009_volFract\t011_contactNum\t   \n";
+  myfileG << "008_vol\t009_volFract\t010_contactNum\t011_vZyldR\t012_vZyldZ\t013_vZyldF\t";
+  myfileG << "014_ShearRate\t015_I\t016_Eta\t017_omega\t018_press\t019_tau\t020_mu\t";
+  myfileG << "021_strTensRR\t022_strTensRZ\t023_strTensRF\t";
+  myfileG << "024_strTensZR\t025_strTensZZ\t026_strTensZF\t";
+  myfileG << "027_strTensFR\t028_strTensFZ\t029_strTensFF\t";
+  myfileG << "030_ShearBand\t \n";
   for(unsigned int b=0; b<_bandRow->size(); b++) {
     std::shared_ptr<band> bT = _bandRow->getBand(b);
     myfileG << bT->id() << "\t";           // 000_id
@@ -448,6 +453,26 @@ void exportclass::gnuplotSchearRate() {
     myfileG << bT->vol() << "\t";          // 008_vol
     myfileG << bT->volFraction() << "\t";  // 009_volFract
     myfileG << bT->contactNumAVG() << "\t";// 010_contactNum
+    myfileG << bT->vZyl()[0]<< "\t";       // 011_vZyldR
+    myfileG << bT->vZyl()[1]<< "\t";       // 012_vZyldZ
+    myfileG << bT->vZyl()[2]<< "\t";       // 013_vZyldF
+    myfileG << bT->scherRate()<< "\t";     // 014_ShearRate
+    myfileG << bT->I()<< "\t";             // 015_I
+    myfileG << bT->eta()<< "\t";           // 016_Eta
+    myfileG << bT->omega()<< "\t";         // 017_omega
+    myfileG << bT->press()<< "\t";         // 018_press
+    myfileG << bT->tau()<< "\t";           // 019_tau
+    myfileG << bT->muAVG()<< "\t";         // 020_mu
+    myfileG << bT->TensorAVG()(0)<< "\t";  // 021_strTensRR
+    myfileG << bT->TensorAVG()(1)<< "\t";  // 022_strTensRZ
+    myfileG << bT->TensorAVG()(2)<< "\t";  // 023_strTensRF
+    myfileG << bT->TensorAVG()(3)<< "\t";  // 024_strTensZR
+    myfileG << bT->TensorAVG()(4)<< "\t";  // 025_strTensZZ
+    myfileG << bT->TensorAVG()(5)<< "\t";  // 026_strTensZF
+    myfileG << bT->TensorAVG()(6)<< "\t";  // 027_strTensFR
+    myfileG << bT->TensorAVG()(7)<< "\t";  // 028_strTensFZ
+    myfileG << bT->TensorAVG()(8)<< "\t";  // 029_strTensFF
+    myfileG << bT->shearBand()<< "\t";     // 030_ShearBand - True (1) or False (0)
     myfileG << " \n"; 
   }
         
