19f831745e2c75bd06ccb30d1cc8da4f6174fce2
Anton Gladky <gladky.anton@gmail.com>
2015-04-16 19:40:24 +0200

    Update FindALGLIB.cmake

--- cMake/FindALGLIB.cmake
+++ cMake/FindALGLIB.cmake
@@ -1,10 +1,13 @@
-# - Try to find agllib 
+# - Try to find alglib 
 # Once done this will define
-#  ALGLIB_INCLUDE_DIRS - The octave include directories
-#  ALGLIB_LIB - The libraries needed to use octave
+#  ALGLIB_INCLUDE_DIRS - The alglib include directory
+#  ALGLIB_LIB - The libraries needed to use alglib
 #  ALGLIB_FOUND -  True if ALGLIB found.
 
-find_path (ALGLIB_INCLUDE_DIRS ap.h)
+find_path (ALGLIB_INCLUDE_DIRS 
+  ap.h
+  PATHS ${CMAKE_INSTALL_PREFIX}/include
+  PATH_SUFFIXES libalglib)
 find_library (ALGLIB_LIB NAMES alglib)
 
 include (FindPackageHandleStandardArgs)
